6e7fe1327704445129ebf0b0b62407270aea2862
rayanMishra <rayanmishra7@gmail.com>
2023-05-08 08:39:01 -0400

    minor fixes

--- script/script.js
+++ script/script.js
@@ -1,7 +1,15 @@
 const hamburger = document.querySelector('.nav--burger');
 const navMenu = document.querySelector('.navbar__hamburger');
+const list = document.querySelectorAll('.hamburger__link');
 
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   navMenu.classList.toggle('navbar__list--active');
+});
+
+list.forEach((link) => {
+  link.addEventListener('click', () => {
+    navMenu.classList.remove('navbar__list--active');
+    hamburger.classList.toggle('active');
+  });
 });
